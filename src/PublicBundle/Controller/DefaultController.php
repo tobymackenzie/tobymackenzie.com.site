3c7df9f9dcb9e5c5dbaefe84c08f4376395ba377
Toby Mackenzie <public@tobymackenzie.com>
2017-12-08 01:32:26 -0500

    revert absolute path change, problem wasn't relevant on live site

--- src/PublicBundle/Controller/DefaultController.php
+++ src/PublicBundle/Controller/DefaultController.php
@@ -4,7 +4,6 @@ use DateTime;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class DefaultController extends Controller{
 	public function simpleFilePageAction(Request $request, $id){
@@ -81,8 +80,7 @@ class DefaultController extends Controller{
 		}
 		foreach($possibleFormats as $format){
 			if($request->getRequestFormat() !== $format){
-				//-! we're making this absolute because otherwise symfony is outputting them as protocol-less absolute.  This won't happen if we can get rid of the host in the routing.
-				$data['formats'][$format] = $this->get('router')->generate($routeName, ['_format'=> $format], UrlGeneratorInterface::ABSOLUTE_URL);
+				$data['formats'][$format] = $this->get('router')->generate($routeName, ['_format'=> $format]);
 			}
 		}
 		$response = $this->renderPage('PublicBundle:default:simplePage.' . $request->getRequestFormat() . '.twig', $data);
