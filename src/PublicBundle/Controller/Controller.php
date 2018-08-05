d50b8f22ea606524d7c3631da0aba4de423db8b9
Toby Mackenzie <public@tobymackenzie.com>
2018-08-05 00:23:20 -0400

    create views service, store default data for template so we can use it elsewhere

--- src/PublicBundle/Controller/Controller.php
+++ src/PublicBundle/Controller/Controller.php
@@ -5,56 +5,8 @@ use TJM\Bundle\BaseBundle\Controller\Controller as Base;
 
 class Controller extends Base{
 	protected function getGlobalRenderData(array $parameters = Array()){
-		if(!isset($parameters["doc"])){
-			$parameters["doc"] = [];
-		}
-		if(!isset($parameters["doc"]['name'])){
-			$parameters["doc"]['name'] = null;
-		}
-		if(!array_key_exists("page", $parameters)){
-			$parameters["page"] = Array();
-		}
-		if(!array_key_exists("wrap", $parameters["page"])){
-			$parameters["page"]['wrap'] = 'public_full';
-		}
-		if(!array_key_exists("site", $parameters)){
-			$parameters["site"] = Array();
-		}
-		if(!array_key_exists("isResponsive", $parameters["site"])){
-			$parameters["site"]['isResponsive'] = true;
-		}
 		$request = $this->get('request_stack')->getMasterRequest();
-		if(isset($parameters['format'])){
-			$format = $parameters['format'];
-		}elseif($request){
-			$format = $request->getRequestFormat();
-		}
-		if(!array_key_exists("title", $parameters["site"])){
-			$parameters["site"]['title'] = '<toby';
-			if($format === 'xhtml'){
-				$parameters["site"]['title'] .= '/';
-			}
-			$parameters["site"]['title'] .= '>';
-		}
-		if($request && $parameters["doc"]['name'] !== 'error'){
-			if(!isset($parameters['canonical']) && ($format === 'html' || $format === 'xhtml')){
-				if($request->getScheme() !== 'https' || $request->getHost() !== 'www.tobymackenzie.com' || $format !== 'html'){
-					$currentRoute = $request->get('_route');
-					if($currentRoute === 'public_home_formatted'){
-						$currentRoute = 'public_home';
-					}elseif($currentRoute === 'public_page_formatted'){
-						$currentRoute = 'public_page';
-					}
-					$routeParams = $request->get('_route_params');
-					unset($routeParams['_format']);
-					$parameters['canonical'] = 'https://www.tobymackenzie.com' . $this->get('router')->generate($currentRoute, $routeParams);
-				}
-			}
-			if($request->getScheme() !== 'https' && !isset($parameters['page']['secureUrl'])){
-				$parameters['page']['secureUrl'] = 'https://' . $request->getHost() . $request->server->get('REQUEST_URI');
-			}
-		}
-		return parent::getGlobalRenderData($parameters);
+		return $this->get('TJM\Views\Views')->getDocData($request, $parameters);
 	}
 	public function renderPage($view, array $parameters = array(), Response $response = null){
 		$parameters = $this->getGlobalRenderData($parameters);
---
+++ src/TJM/Views/Views.php
@@ -0,0 +1,90 @@
+<?php
+namespace TJM\Views;
+use Symfony\Component\HttpFoundation\Request;
+
+class Views{
+	protected $router;
+	protected $wraps;
+	public function __construct($router, array $wraps){
+		$this->router = $router;
+		$this->wraps = $wraps;
+	}
+	public function getDocData(Request $request = null, array $data = null){
+		if(!$data){
+			$data = Array();
+		}
+		if(!isset($data["doc"])){
+			$data["doc"] = [];
+		}
+		if(!isset($data["doc"]['name'])){
+			$data["doc"]['name'] = null;
+		}
+		if(!isset($data["page"])){
+			$data["page"] = Array();
+		}
+		if(!isset($data["site"])){
+			$data["site"] = Array();
+		}
+		if(!isset($data["site"]["isResponsive"])){
+			$data["site"]['isResponsive'] = true;
+		}
+		if(isset($data['format'])){
+			$format = $data['format'];
+		}elseif($request){
+			$format = $request->getRequestFormat();
+		}
+		if(!isset($data["site"]["title"])){
+			$data["site"]['title'] = '<toby';
+			if($format === 'xhtml'){
+				$data["site"]['title'] .= '/';
+			}
+			$data["site"]['title'] .= '>';
+		}
+		if($request && $data["doc"]['name'] !== 'error'){
+			if(!isset($data['canonical']) && ($format === 'html' || $format === 'xhtml')){
+				if($request->getScheme() !== 'https' || $request->getHost() !== 'www.tobymackenzie.com' || $format !== 'html'){
+					$currentRoute = $request->get('_route');
+					if($currentRoute === 'public_home_formatted'){
+						$currentRoute = 'public_home';
+					}elseif($currentRoute === 'public_page_formatted'){
+						$currentRoute = 'public_page';
+					}
+					$routeParams = $request->get('_route_params');
+					unset($routeParams['_format']);
+					$data['canonical'] = 'https://www.tobymackenzie.com' . $this->router->generate($currentRoute, $routeParams);
+				}
+			}
+			if($request->getScheme() !== 'https' && !isset($data['page']['secureUrl'])){
+				$data['page']['secureUrl'] = 'https://' . $request->getHost() . $request->server->get('REQUEST_URI');
+			}
+		}
+		if(!isset($data["page"]["wrap"])){
+			$data["page"]["wrap"] = 'public_full';
+		}
+		if(!isset($data["page"]["skeleton"])){
+			$data["page"]["skeleton"] = $this->wraps[$data["page"]["wrap"]] ?? $this->wraps["public_full"];
+		}
+		//-!! format should come from parameters if set
+		$data['page']['skeleton'] = str_replace('{format}', $format , $data['page']['skeleton']);
+		if(!isset($data['doc'])){
+			$data['doc'] = Array();
+		}
+		if(!isset($data['doc']['attr'])){
+			$data['doc']['attr'] = '';
+		}elseif($data['doc']['attr']){
+			$data['doc']['attr'] .= ' ';
+		}
+		if(!isset($data['doc']['language'])){
+			$data['doc']['language'] = 'en';
+		}
+		if(strpos($data['doc']['attr'], 'lang=') === false){
+			$data['doc']['attr'] .= 'lang="' . $data['doc']['language'] . '"';
+		}
+		if($format === 'xhtml'){
+			if(strpos($data['doc']['attr'], 'xmlns=') === false){
+				$data['doc']['attr'] .= ' xmlns="http://www.w3.org/1999/xhtml" xml:lang="' . $data['doc']['language'] . '"';
+			}
+		}
+		return $data;
+	}
+}
