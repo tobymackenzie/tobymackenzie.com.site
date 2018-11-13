0d915caa9f1d2a06c1ed283c96f3a78fbe4d8850
Toby Mackenzie <public@tobymackenzie.com>
2018-11-13 01:31:19 -0500

    prefix web app entrances with '_'

--- app/web/_app_dev.php
+++ app/web/_app_dev.php
@@ -3,4 +3,4 @@ use TJM\Bundle\StandardEditionBundle\Component\App\App;
 
 $loader = require_once __DIR__ . '/../autoload.php';
 App::setEnvironment('dev');
-require __DIR__ . '/app.php';
+require __DIR__ . '/_app.php';
