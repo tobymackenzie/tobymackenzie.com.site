61cb9ea1db7a77b361a1879d2c8632086e3175df
Toby Mackenzie <public@tobymackenzie.com>
2019-05-19 23:32:45 -0400

    move app environment config to bootstrap, not being used elsewhere

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,22 +1,27 @@
 <?php
 namespace TJM\Bundle\StandardEditionBundle\Component\App;
-
 use Composer\Autoload\ClassLoader;
 
 error_reporting(error_reporting() & ~E_USER_DEPRECATED);
 
-$app = require_once(__DIR__ . '/config/app.php');
-
-/**
-* @var ClassLoader $app['loader']
-*/
-$app['loader'] = require(constant(__NAMESPACE__ . '\VENDOR_DIR') . '/autoload.php');
-
-/*
-Define configuration used by App singleton, such as paths.
-*/
-App::getSingleton($app);
+//--Define configuration used by App singleton, such as paths.
+define(__NAMESPACE__ . '\PROJECT_DIR', realpath(__DIR__ . '/..'));
+define(__NAMESPACE__ . '\VENDOR_DIR', constant(__NAMESPACE__ . '\PROJECT_DIR') . '/vendor');
+$loader = require(constant(__NAMESPACE__ . '\VENDOR_DIR') . '/autoload.php');
+App::getSingleton([
+	'loader'=> $loader
+	,'paths'=> [
+		//--cli paths
+		'PHPCLI'=> '/usr/bin/php'
+		//--symfony paths
+		,'app'=> constant(__NAMESPACE__ . '\PROJECT_DIR') . '/app'
+		,'project'=> constant(__NAMESPACE__ . '\PROJECT_DIR')
+		,'src'=> constant(__NAMESPACE__ . '\PROJECT_DIR') . '/src'
+		,'var'=> constant(__NAMESPACE__ . '\PROJECT_DIR') . '/var'
+		,'vendor'=> constant(__NAMESPACE__ . '\VENDOR_DIR')
+	]
+]);
 //-# uncomment to enable Symfony's HTTPCache in 'prod' environment
 // App::getSingleton()->setCache('prod');
 
-return $app['loader'];
+return App::getLoader();
