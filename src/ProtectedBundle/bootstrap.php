879e51fcf90ca95349632a7f1e2e6a0977bc2792
Toby Mackenzie <public@tobymackenzie.com>
2019-08-10 19:23:39 -0400

    php: remove unnecessary use of `constant`

--- src/ProtectedBundle/bootstrap.php
+++ src/ProtectedBundle/bootstrap.php
@@ -6,8 +6,8 @@ error_reporting(error_reporting() & ~E_USER_DEPRECATED);
 
 //--Define configuration used by App singleton, such as paths.
 define(__NAMESPACE__ . '\PROJECT_DIR', realpath(__DIR__ . '/../..'));
-define(__NAMESPACE__ . '\VENDOR_DIR', constant(__NAMESPACE__ . '\PROJECT_DIR') . '/vendor');
-$loader = require(constant(__NAMESPACE__ . '\VENDOR_DIR') . '/autoload.php');
+define(__NAMESPACE__ . '\VENDOR_DIR', PROJECT_DIR . '/vendor');
+$loader = require(VENDOR_DIR . '/autoload.php');
 $app = new App([
 	'bundles'=> [
 		'Symfony\Bundle\FrameworkBundle\FrameworkBundle'
@@ -27,10 +27,11 @@ $app = new App([
 	,'paths'=> [
 		//--symfony paths
 		'app'=> __DIR__
-		,'project'=> constant(__NAMESPACE__ . '\PROJECT_DIR')
-		,'src'=> constant(__NAMESPACE__ . '\PROJECT_DIR') . '/src'
-		,'var'=> constant(__NAMESPACE__ . '\PROJECT_DIR') . '/var'
-		,'vendor'=> constant(__NAMESPACE__ . '\VENDOR_DIR')
+		,'project'=> PROJECT_DIR
+		,'src'=> PROJECT_DIR . '/src'
+		,'var'=> PROJECT_DIR . '/var'
+		,'vendor'=> VENDOR_DIR
+
 	]
 ]);
 
