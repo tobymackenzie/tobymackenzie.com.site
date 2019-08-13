26a73ee378d32f6da0e5b9c362153a0071f948f0
Toby Mackenzie <public@tobymackenzie.com>
2019-08-12 23:11:39 -0400

    blog must use `twig` instead of deprecated `templating` service

--- src/Blog/theme/footer.php
+++ src/Blog/theme/footer.php
@@ -3,4 +3,4 @@ SymfonyHelper::$viewData['content'] = ob_get_contents();
 ob_end_clean();
 //-! what to do with sidebar?
 // get_sidebar();
-echo SymfonyHelper::getService('templating')->render('@Public/content.html.twig', SymfonyHelper::getViewData());
+echo SymfonyHelper::getService('twig')->render('@Public/content.html.twig', SymfonyHelper::getViewData());
