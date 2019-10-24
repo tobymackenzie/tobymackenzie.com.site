16808d19a08d2a29ff0057e60ac5307c5d29ee94
Toby Mackenzie <public@tobymackenzie.com>
2019-10-24 01:06:57 -0400

    ensure app nav shadow doesn't make room for top nav unless it's there

--- src/PublicApp/Resources/scripts/modules/appNav.js
+++ src/PublicApp/Resources/scripts/modules/appNav.js
@@ -17,11 +17,15 @@ if(
 	_topNavItem.className += ' appRestartNav';
 	_topNavItem.innerHTML = '<a class="appRestartAction appNavAction" href="#top" id="top-link"><span class="appNavItemText"><span class="appNavItemHash">#</span>Page top</span></a>';
 	_appEl.appendChild(_topNavItem);
-	var _navList = document.querySelector('.appNavList');
-	if(_navList){
-		var _bottomNavItem = document.createElement('li');
-		_bottomNavItem.innerHTML = '<a class="appBottomAction appNavAction" href="#bottom" id="bottom-link"><span class="appNavItemText"><span class="appNavItemHash">#</span>Page bottom</span></a>';
-		_navList.appendChild(_bottomNavItem);
+	var _nav = document.querySelector('.appNav');
+	if(_nav){
+		_nav.setAttribute('data-js', 1);
+		var _navList = _nav.querySelector('.appNavList');
+		if(_navList){
+			var _bottomNavItem = document.createElement('li');
 			_bottomNavItem.className += 'appNavItem';
+			_bottomNavItem.innerHTML = '<a class="appBottomAction appNavAction" href="#bottom" id="bottom-link"><span class="appNavItemText"><span class="appNavItemHash">#</span>Page bottom</span></a>';
+			_navList.appendChild(_bottomNavItem);
+		}
 	}
 }
