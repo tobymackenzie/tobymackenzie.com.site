bcd68d968124cbbd8ec3956d4e607119b9902aca
Toby Mackenzie <public@tobymackenzie.com>
2022-01-09 16:18:14 -0500

    perf: ensure js load of css is non-blocking

--- src/PublicApp/scripts/ua/load-css.js
+++ src/PublicApp/scripts/ua/load-css.js
@@ -5,11 +5,26 @@ export default function loadCSS(_src, _props, _insertBefore){
 	var _link = _d.createElement('link');
 	_link.href = _src;
 	_link.rel = 'stylesheet';
+	var _media = 'all';
+	var _onload;
 	//-! we probably want to be able to set attributes instead of props
 	if(typeof _props === 'function'){
 		_props(_script);
 	}else if(typeof _props === 'object' && Object.assign){
+		if(_props.media){
+			_media = _props.media;
+		}
+		if(_props.onload){
+			_onload = _props.onload;
+		}
 		Object.assign(_link, _props);
+	}
+	_link.media = 'only x';
+	_link.onload = function(){
+		_link.media = _media;
+		if(_onload){
+			_onload();
+		}
 	}
 	if(_insertBefore){
 		_insertBefore.parentNode.insertBefore(_link, _insertBefore);
