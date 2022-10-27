a955fc82bd8506e9c1d6d271b4a800bac2706bb8
Toby Mackenzie <public@tobymackenzie.com>
2022-10-27 01:15:16 -0400

    whimsy: add messages for various holidays

--- src/PublicApp/scripts/modules/extra.js
+++ src/PublicApp/scripts/modules/extra.js
@@ -25,4 +25,28 @@ if(window.Date){
 	if(month === 3 && day === 1){
 		loadJS('/_assets/scripts/aprilFools.js');
 	}
+	//--message for date
+	//-# too small to put in separate files
+	var messages = {
+		'0101': 'Happy New Year 🎉',
+		'0106': 'Happy Toby Day',
+		'0401': 'loof, lirpA ot emocleW',
+		//-! Easter has complicated algorithm: https://stackoverflow.com/a/44480326
+		'0422': '🌎 🌳',
+		'0504': 'May the 4th be with you',
+		'0505': 'May the 5th be with you 🌮',
+		'0521': '☕️',
+		'0704': 'Happy independence 🎆',
+		'1031': 'Happy Halloween ',
+		'1105': 'Remember, remember the 5th of November',
+		'1225': 'Merry Christmas 🎄',
+	};
+	var year = now.getFullYear();
+	var novDay = new Date(year, 10, 1).getDay();
+	messages['11' + ((novDay > 4 ? 33 : 26) - novDay)] = 'Thanks 🦃';
+	var dayString = ((month + 1).toString().padStart(2, '0')) + (day.toString().padStart(2, '0'));
+	var messageEl;
+	if(messages[dayString] && document.querySelector && (messageEl = document.querySelector('.appHeaderMessage'))){
+		messageEl.innerHTML = messages[dayString];
+	}
 }
