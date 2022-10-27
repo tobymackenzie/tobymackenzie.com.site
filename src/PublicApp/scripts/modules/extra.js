dd018cb55f2dcb45f61a42575539cb3b0eafa1a3
Toby Mackenzie <public@tobymackenzie.com>
2022-10-27 01:12:04 -0400

    fix(extra): js module loading wasn't working properly

--- src/PublicApp/scripts/modules/extra.js
+++ src/PublicApp/scripts/modules/extra.js
@@ -1,5 +1,5 @@
-import loadCSS from '../ua/load-css';
-import loadJS from '../ua/load-js';
+import loadCSS from '../ua/load-css.js';
+import loadJS from '../ua/load-js.js';
 
 //--load christmas script if december or early january
 //-! simple onload.  we may want to have it run repeatedly and enable / disable as necessary
