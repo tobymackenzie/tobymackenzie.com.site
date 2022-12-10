1c1550dc6f0689c426d619879fbafa2f8bd74c74
Toby Mackenzie <public@tobymackenzie.com>
2022-12-09 23:22:46 -0500

    fix(whimsy): direct load whimsy scripts from tobymackenzie.com

--- src/PublicApp/scripts/modules/extra.js
+++ src/PublicApp/scripts/modules/extra.js
@@ -4,6 +4,7 @@ import loadJS from '../ua/load-js.js';
 //--load christmas script if december or early january
 //-! simple onload.  we may want to have it run repeatedly and enable / disable as necessary
 if(window.Date){
+	var assetBase = (window.location.host.match(/github\.io|macn\.me$/i) ? '//www.tobymackenzie.com' : '');
 	var now = new Date();
 	var month = now.getMonth();
 	var day = now.getDate();
@@ -12,18 +13,18 @@ if(window.Date){
 		month === 11
 		|| month < 2
 	){
-		loadCSS('/_assets/styles/snow.css');
-		loadJS('/_assets/scripts/snow.js');
+		loadCSS(assetBase + '/_assets/styles/snow.css');
+		loadJS(assetBase + '/_assets/scripts/snow.js');
 	}
 	if(month === 9 && day >= 22){
-		loadCSS('/_assets/styles/halloween.css');
-		loadJS('/_assets/scripts/halloween.js');
+		loadCSS(assetBase + '/_assets/styles/halloween.css');
+		loadJS(assetBase + '/_assets/scripts/halloween.js');
 	}
 	if(month === 11){
-		loadCSS('/_assets/styles/christmas.css');
+		loadCSS(assetBase + '/_assets/styles/christmas.css');
 	}
 	if(month === 3 && day === 1){
-		loadJS('/_assets/scripts/aprilFools.js');
+		loadJS(assetBase + '/_assets/scripts/aprilFools.js');
 	}
 
 	//--message for date
