690c7011c5084e6350fc29964764618ebde07eb3
Toby Mackenzie <public@tobymackenzie.com>
2022-12-09 22:57:38 -0500

    build(card): card js now in ShortApp folder and build

--- src/ShortApp/inc.php
+++ src/ShortApp/inc.php
@@ -7,9 +7,10 @@ define('AUTOLOAD_PATH', (getenv('GITHUB_ACTIONS') && getenv('GITHUB_WORKSPACE') 
 
 define('SHORT_PATH', realpath(__DIR__));
 define('INDEX_PATH', SHORT_PATH . '/index.php');
+define('JS_PATH', SHORT_PATH . '/scripts.js');
 define('STYLES_PATH', SHORT_PATH . '/styles.css');
 
 define('DIST_PATH', getenv('GITHUB_ACTIONS') && getenv('GITHUB_WORKSPACE') ? getenv('GITHUB_WORKSPACE') . '/dist' : PROJECT_PATH . '/dist/short');
 define('INDEX_DIST_PATH', DIST_PATH . '/index.html');
-define('JS_DIST_PATH', DIST_PATH . '/../public/_assets/scripts/short.js');
+define('JS_DIST_PATH', DIST_PATH . '/_scripts.js');
 define('STYLES_DIST_PATH', DIST_PATH . '/_styles.css');
---
+++ src/ShortApp/scripts.js
@@ -0,0 +1,2 @@
+import * as extra from '../PublicApp/scripts/modules/extra.js';
+import * as konamiCode from '../PublicApp/scripts/modules/konamiCode.js';
