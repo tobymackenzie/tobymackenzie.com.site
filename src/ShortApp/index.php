317e9bd314bd8977d2f6df14d6085aa40136f8d0
Toby Mackenzie <public@tobymackenzie.com>
2022-12-09 22:59:21 -0500

    code(short): move index scripts into main script file

--- src/ShortApp/index.php
+++ src/ShortApp/index.php
@@ -63,40 +63,6 @@ if(file_exists($jsFile)){
 ?>
 		<script><!--
 		<?=file_get_contents($jsFile)?>
-		(function(){
-			//-!! forcing disable in safari because it's not working currently: back disappearing
-			if(document.querySelector && window.CSS && CSS.supports && CSS.supports('backface-visibility: visible') && !(/^((?!chrome|android).)*safari/i.test(navigator.userAgent))){
-				var $card = document.querySelector('.card');
-				var $cardFaces = $card.querySelectorAll('.cardFace');
-				for(var i = 0; i < $cardFaces.length; ++i){
-					var $button = document.createElement('button');
-					$button.innerHTML = '<span>flip</span>';
-					$button.classList.add('cardFlip');
-					$button.addEventListener('click', function(){
-						if($card.dataset.show === 'back'){
-							$card.dataset.show = 'front';
-						}else{
-							$card.dataset.show = 'back';
-						}
-					});
-					$cardFaces[i].appendChild($button);
-				}
-			}
-		})();
-		//--hide darken of me image from neobrowser, possibly other old Chromium, otherwise crashes on load (aw snap)
-		(function(){
-			var matches;
-			if(
-				document.querySelector && window.CSS && CSS.supports && CSS.supports('mix-blend-mode: darken')
-				&& !(
-					(matches = navigator.userAgent.match(/Chrome\/([^ \.]+)/))
-					&& matches
-					&& parseInt(matches[1], 10) <= 85
-				)
-			){
-				document.querySelector('html').classList.add('supports-darken')
-			}
-		})();
 		--></script>
 <?php } ?>
 	</body>
--- src/ShortApp/scripts.js
+++ src/ShortApp/scripts.js
@@ -1,2 +1,38 @@
 import * as extra from '../PublicApp/scripts/modules/extra.js';
 import * as konamiCode from '../PublicApp/scripts/modules/konamiCode.js';
+
+//-!! forcing disable in safari because it's not working currently: back disappearing
+(function(){
+	if(document.querySelector && window.CSS && CSS.supports && CSS.supports('backface-visibility: visible') && !(/^((?!chrome|android).)*safari/i.test(navigator.userAgent))){
+		var $card = document.querySelector('.card');
+		var $cardFaces = $card.querySelectorAll('.cardFace');
+		for(var i = 0; i < $cardFaces.length; ++i){
+			var $button = document.createElement('button');
+			$button.innerHTML = '<span>flip</span>';
+			$button.classList.add('cardFlip');
+			$button.addEventListener('click', function(){
+				if($card.dataset.show === 'back'){
+					$card.dataset.show = 'front';
+				}else{
+					$card.dataset.show = 'back';
+				}
+			});
+			$cardFaces[i].appendChild($button);
+		}
+	}
+})();
+
+//--hide darken of me image from neobrowser, possibly other old Chromium, otherwise crashes on load (aw snap)
+(function(){
+	var matches;
+	if(
+		document.querySelector && window.CSS && CSS.supports && CSS.supports('mix-blend-mode: darken')
+		&& !(
+			(matches = navigator.userAgent.match(/Chrome\/([^ \.]+)/))
+			&& matches
+			&& parseInt(matches[1], 10) <= 85
+		)
+	){
+		document.querySelector('html').classList.add('supports-darken')
+	}
+})();
