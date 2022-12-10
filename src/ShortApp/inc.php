601c04e4e6925bb618e94ffc6abf6695461a0cf7
Toby Mackenzie <public@tobymackenzie.com>
2022-12-09 19:16:07 -0500

    build: support building into github dist folder

--- src/ShortApp/inc.php
+++ src/ShortApp/inc.php
@@ -9,7 +9,7 @@ define('SHORT_PATH', realpath(__DIR__));
 define('INDEX_PATH', SHORT_PATH . '/index.php');
 define('STYLES_PATH', SHORT_PATH . '/styles.css');
 
-define('DIST_PATH', PROJECT_PATH . '/dist/short');
+define('DIST_PATH', getenv('GITHUB_ACTIONS') && getenv('GITHUB_WORKSPACE') ? getenv('GITHUB_WORKSPACE') . '/dist' : PROJECT_PATH . '/dist/short');
 define('INDEX_DIST_PATH', DIST_PATH . '/index.html');
 define('JS_DIST_PATH', DIST_PATH . '/../public/_assets/scripts/short.js');
 define('STYLES_DIST_PATH', DIST_PATH . '/_styles.css');
