0b6081734bcd8b17743426e5d7c8ef08b3f020b4
Toby Mackenzie <public@tobymackenzie.com>
2023-01-13 15:43:42 -0500

    dev: new dev vhost IP for new virtualbox version

--- src/PublicApp/bootstrap.php
+++ src/PublicApp/bootstrap.php
@@ -6,7 +6,7 @@ define(__NAMESPACE__ . '\PROJECT_DIR', realpath(__DIR__ . '/../..'));
 define(__NAMESPACE__ . '\VENDOR_DIR', PROJECT_DIR . '/vendor');
 $loader = require(VENDOR_DIR . '/autoload.php');
 $app = new App([
-	'allowedDevIPs'=> ['127.0.0.1', '::1', '10.9.9.1'],
+	'allowedDevIPs'=> ['127.0.0.1', '::1', '192.168.56.1'],
 	'bundles'=> [
 		'Symfony\Bundle\FrameworkBundle\FrameworkBundle'
 		,'Symfony\Bundle\TwigBundle\TwigBundle'
