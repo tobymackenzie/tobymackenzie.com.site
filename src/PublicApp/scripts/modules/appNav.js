f0cda622b90c3a6ea03bb85e7ee2ab899eb661c8
Toby Mackenzie <public@tobymackenzie.com>
2023-01-22 15:20:56 -0500

    feat(maintenance): allow styles / js to show / run on maintenance page

--- src/PublicApp/scripts/modules/appNav.js
+++ src/PublicApp/scripts/modules/appNav.js
@@ -14,10 +14,6 @@ if(
 	--insert top / bottom nav actions
 	adding here to reduce markup on page load
 	*/
-	var _topNavItem = document.createElement('div');
-	_topNavItem.className += ' appRestartNav';
-	_topNavItem.innerHTML = '<a class="appRestartAction appNavAction" href="#top" id="top-link"><span class="appNavItemText"><span class="appNavItemHash">#</span>Page top</span></a>';
-	_appEl.appendChild(_topNavItem);
 	var _nav = document.querySelector('.appNav');
 	if(_nav){
 		_nav.setAttribute('data-js', 1);
@@ -27,6 +23,11 @@ if(
 			_bottomNavItem.className += 'appNavItem';
 			_bottomNavItem.innerHTML = '<a class="appBottomAction appNavAction" href="#bottom" id="bottom-link"><span class="appNavItemText"><span class="appNavItemHash">#</span>Page bottom</span></a>';
 			_navList.appendChild(_bottomNavItem);
+
+			var _topNavItem = document.createElement('div');
+			_topNavItem.className += ' appRestartNav';
+			_topNavItem.innerHTML = '<a class="appRestartAction appNavAction" href="#top" id="top-link"><span class="appNavItemText"><span class="appNavItemHash">#</span>Page top</span></a>';
+			_appEl.appendChild(_topNavItem);
 		}
 	}
 
