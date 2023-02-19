c1130f1b623d72b9aafce12982da424cd9929020
Toby Mackenzie <public@tobymackenzie.com>
2023-02-19 16:59:43 -0500

    tests: add command to run public site unit tests

---
+++ src/PublicApp/Command/TestCommand.php
@@ -0,0 +1,18 @@
+<?php
+namespace PublicApp\Command;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class TestCommand extends Command{
+	protected function configure(){
+		$this
+			->setName('test')
+			->setDescription("Run PHPUnit tests.")
+		;
+	}
+	protected function execute(InputInterface $input, OutputInterface $output): int{
+		passthru('cd ' . __DIR__ . '/.. && phpunit');
+		return 0;
+	}
+}
