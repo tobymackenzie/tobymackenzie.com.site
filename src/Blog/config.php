69eb632c86a805505d2612196448768649578fd3
Toby Mackenzie <public@tobymackenzie.com>
2023-03-11 20:50:41 -0500

    code(blog): don't use global variables in wp config file

--- src/Blog/config.php
+++ src/Blog/config.php
@@ -1,55 +1,54 @@
 <?php
+namespace TJM\Blog;
 use TJM\Component\Utils\Arrays;
 use TJM\WPThemeHelper\ConfigHelper;
 
-$projectPath = dirname(realpath(__FILE__)) . '/../..';
-require_once($projectPath . '/vendor/autoload.php');
+define(__NAMESPACE__ . '\\PROJECT_PATH', dirname(realpath(__FILE__)) . '/../..');
+require_once(PROJECT_PATH . '/vendor/autoload.php');
 
-$config = [
-	'constants'=> [
-		//--cookies
-		'ADMIN_COOKIE_PATH'=> '/'
+ConfigHelper::init((function(){
+	$config = [
+		'constants'=> [
+			//--cookies
+			'ADMIN_COOKIE_PATH'=> '/',
 
-		//--paths
-		,'ABSPATH'=> dirname(realpath(__FILE__)) . '/../../../vendor/wp/wordpress/'
-		,'WP_CONTENT_DIR'=> "{{ConfigHelper::vars.webRootPath}}/{{ConfigHelper::vars.contentRelativePath}}"
+			//--paths
+			'ABSPATH'=> dirname(realpath(__FILE__)) . '/../../../vendor/wp/wordpress/',
+			'WP_CONTENT_DIR'=> "{{ConfigHelper::vars.webRootPath}}/{{ConfigHelper::vars.contentRelativePath}}",
 
-		//--urls
-		,'WP_HOME'=> '{{ConfigHelper::vars.webRootUrl}}/blog'
-		,'WP_CONTENT_URL'=> "{{ConfigHelper::vars.webRootUrl}}/{{ConfigHelper::vars.contentRelativePath}}"
-		,'WP_SITEURL'=> "{{ConfigHelper::vars.webRootUrl}}/{{ConfigHelper::vars.wpRelativePath}}"
+			//--urls
+			'WP_HOME'=> '{{ConfigHelper::vars.webRootUrl}}/blog',
+			'WP_CONTENT_URL'=> "{{ConfigHelper::vars.webRootUrl}}/{{ConfigHelper::vars.contentRelativePath}}",
+			'WP_SITEURL'=> "{{ConfigHelper::vars.webRootUrl}}/{{ConfigHelper::vars.wpRelativePath}}",
 
-		//--security
-		,'AUTH_KEY'=> '!!12345'
-		,'DISALLOW_FILE_EDIT'=> true
-		,'SECURE_AUTH_KEY'=> '!!12345'
-		,'LOGGED_IN_KEY'=> '!!12345'
-		,'NONCE_KEY'=> '!!12345'
-		,'AUTH_SALT'=> '!!12345'
-		,'SECURE_AUTH_SALT'=> '!!12345'
-		,'LOGGED_IN_SALT'=> '!!12345'
-		,'NONCE_SALT'=> '!!12345'
-	]
-	,'globals'=> [
-		'table_prefix'=> '15wp_'
-	]
-	,'vars'=> [
-		'contentRelativePath'=> '_/wp-content'
-		// ,'debug'=> true
-		,'webRootPath'=> $projectPath . '/dist/public'
-		,'webRootUrl'=> '{{ConfigHelper::vars.protocol}}://{{ConfigHelper::vars.host}}'
-		,'wpRelativePath'=> '_/wp'
-	]
-];
-$local = $projectPath . '/config/blog.local.php';
-if(file_exists($local)){
-	$config = Arrays::deepMerge($config, include($local));
-}
-ConfigHelper::init($config);
-unset($config);
-unset($local);
-unset($projectPath);
-
+			//--security
+			'AUTH_KEY'=> '!!12345',
+			'DISALLOW_FILE_EDIT'=> true,
+			'SECURE_AUTH_KEY'=> '!!12345',
+			'LOGGED_IN_KEY'=> '!!12345',
+			'NONCE_KEY'=> '!!12345',
+			'AUTH_SALT'=> '!!12345',
+			'SECURE_AUTH_SALT'=> '!!12345',
+			'LOGGED_IN_SALT'=> '!!12345',
+			'NONCE_SALT'=> '!!12345',
+		],
+		'globals'=> [
+			'table_prefix'=> '15wp_',
+		],
+		'vars'=> [
+			'contentRelativePath'=> '_/wp-content',
+			// 'debug'=> true,
+			'webRootPath'=> PROJECT_PATH . '/dist/public',
+			'webRootUrl'=> '{{ConfigHelper::vars.protocol}}://{{ConfigHelper::vars.host}}',
+			'wpRelativePath'=> '_/wp',
+		],
+	];
+	$local = PROJECT_PATH . '/config/blog.local.php';
+	if(file_exists($local)){
+		$config = Arrays::deepMerge($config, include($local));
+	}
+	return $config;
+})());
 if(!isset($table_prefix) && isset($GLOBALS['table_prefix'])){
 	$table_prefix = $GLOBALS['table_prefix'];
 }
