fa9234c350ac2c2536b750363b6de87979e53270
Toby Mackenzie <public@tobymackenzie.com>
2023-10-01 15:55:55 -0400

    feat: allow passing options to command line diff of `web:diff` command

--- src/PublicApp/Command/WebDiffCommand.php
+++ src/PublicApp/Command/WebDiffCommand.php
@@ -3,6 +3,7 @@ namespace PublicApp\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -13,6 +14,7 @@ class WebDiffCommand extends Command{
 			->setDescription("Compare output response for two different requests.")
 			->addArgument('pathA', InputArgument::REQUIRED, 'URL path to request')
 			->addArgument('pathB', InputArgument::REQUIRED, 'Other URL path to request')
+			->addOption('opts', 'o', InputOption::VALUE_REQUIRED, 'Options to pass to `git diff` command.')
 		;
 	}
 	protected function execute(InputInterface $input, OutputInterface $output): int{
@@ -29,9 +31,9 @@ class WebDiffCommand extends Command{
 		file_put_contents($outB, (string) $response);
 		//-! maybe use symfony process instead of passthru() to get testable output and possibly smarter color handling, etc
 		if(`which git`){
-			passthru("git diff --no-index --color=always -w {$outA} {$outB}");
+			passthru("git diff " . $input->getOption('opts') . " --no-index --color=always -w {$outA} {$outB}");
 		}else{
-			passthru("diff {$outA} {$outB}");
+			passthru("diff " . $input->getOption('opts') . " {$outA} {$outB}");
 		}
 		unlink($outA);
 		unlink($outB);
