075192a5f3030cb5ff25d31376afc9c19b6485d4
Toby Mackenzie <public@tobymackenzie.com>
2023-10-01 23:36:34 -0400

    code: remove unused `use` statements

--- src/PublicApp/Command/WebDiffCommand.php
+++ src/PublicApp/Command/WebDiffCommand.php
@@ -5,7 +5,6 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\HttpFoundation\Request;
 
 class WebDiffCommand extends Command{
 	protected function configure(){
