f160c23b302e21d14c33eca6efac158f9de35ed6
Toby Mackenzie <public@tobymackenzie.com>
2024-07-27 12:11:09 -0400

    code: simplify bootstrap, pare down what is needed

--- src/PublicApp/bootstrap.php
+++ src/PublicApp/bootstrap.php
@@ -1,11 +1,11 @@
 <?php
 namespace TJM\SyWeb;
 
-//--Define configuration used by App singleton, such as paths.
-define(__NAMESPACE__ . '\APP_DIR', realpath(__DIR__));
-define(__NAMESPACE__ . '\PROJECT_DIR', realpath(APP_DIR . '/../..'));
-define(__NAMESPACE__ . '\VENDOR_DIR', PROJECT_DIR . '/vendor');
-$loader = require(VENDOR_DIR . '/autoload.php');
+//--configure paths
+const APP_DIR = __DIR__;
+const PROJECT_DIR = APP_DIR . '/../..';
+require(PROJECT_DIR . '/vendor/autoload.php');
+//--build app
 $app = new App([
 	'allowedDevIPs'=> ['127.0.0.1', '::1', '192.168.56.1'],
 	'bundles'=> [
@@ -17,16 +17,9 @@ $app = new App([
 		'Symfony\Bundle\DebugBundle\DebugBundle'=> ['dev', 'test'],
 		'Symfony\Bundle\WebProfilerBundle\WebProfilerBundle'=> ['dev', 'test'],
 	],
-	//-# uncomment to enable Symfony's HTTPCache in 'prod' environment
-	// 'cache'=> 'prod',
-	'loader'=> $loader,
 	'paths'=> [
-		//--symfony paths
-		'app'=> __DIR__,
+		'app'=> APP_DIR,
 		'project'=> PROJECT_DIR,
-		'src'=> PROJECT_DIR . '/src',
-		'var'=> PROJECT_DIR . '/var',
-		'vendor'=> VENDOR_DIR,
 	],
 ]);
 return $app;
