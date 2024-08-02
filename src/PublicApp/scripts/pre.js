eeadd652bcc716fddde61db67c799eec5e6ffe71
Toby Mackenzie <public@tobymackenzie.com>
2024-08-02 14:49:15 -0400

    feat: canonical redirect script now won't use canonical tag so we can specify HTML as canonical format.  Also simplify

--- src/PublicApp/scripts/pre.js
+++ src/PublicApp/scripts/pre.js
@@ -3,31 +3,29 @@ import loadJS from './ua/load-js.js';
 /*--force canonical domain from IP, or http to https
 - forcing by loading script ensures that browser can actually load it, unlike with server forced redirect
 - putting in pre reduces rendering / loading of http content if redirect happens
-- do we need secure box on page with this?
 */
-var $canonical;
-var win = window;
-var doc = win.document;
-var loc = 'location' in win ? win.location : null;
-if(
-	loc
-	&& win.URL
-	&& doc.querySelector
-	&& ($canonical = doc.querySelector('link[rel="canonical"]'))
-	&& $canonical
-	&& $canonical.classList
-	&& !doc.querySelector('html').classList.contains('env-dev')
-	//--rough check if visiting via IP
-	&& (
-		loc.host.match(/^([\d\.]+|[\w:]+:[\w:]+)$/)
-		|| (loc.protocol === 'http:' && $canonical.href.slice(5) === 'https')
-	)
-){
-	var url = new URL($canonical.href);
-	if(win.TJMFRCR){
-		win.location = url.origin + loc.pathname + loc.search + loc.hash;
-	}else{
-		win.TJMFRCR = 1;
-		loadJS(url.origin + '/_assets/scripts/pre.js');
+var w = window;
+if('location' in w){
+	var canonicalHost = 'www.tobymackenzie.com'
+	var l = location;
+	var origin, url;
+	//--attempt force canonicalHost if IP access
+	//-# rough check if visiting via IP
+	if(l.host !== canonicalHost && l.host.match(/^([\d\.]+|[\w:]+:[\w:]+)$/)){
+		origin = l.protocol + '//' + canonicalHost;
+		url = origin + l.pathname + l.search + l.hash;
+	}
+	//--attempt force https if http
+	else if(l.protocol === 'http:'){
+		origin = 'https://' + l.host
+		url = origin + l.pathname + l.search + l.hash;
+	}
+	if(url){
+		if(w.TJMFRCR){
+			w.location = url;
+		}else{
+			w.TJMFRCR = 1;
+			loadJS(origin + '/_assets/scripts/pre.js');
+		}
 	}
 }
