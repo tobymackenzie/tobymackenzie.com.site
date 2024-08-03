b7eee8f64575990af6de115a12294c9e3a99b582
Toby Mackenzie <public@tobymackenzie.com>
2024-08-03 16:57:42 -0400

    feat: combine all build / assets commands into one, run all with default `build` command

--- src/PublicApp/Command/BuildCommand.php
+++ src/PublicApp/Command/BuildCommand.php
@@ -9,14 +9,27 @@ use PublicApp\Service\Build;
 
 class BuildCommand extends Command{
 	protected $buildService;
+	const TASKS = [
+		'assets',
+		'css',
+		'js',
+		'static',
+		'webroot',
+		'svg',
+	];
 	public function __construct(Build $buildService){
 		$this->buildService = $buildService;
 		parent::__construct();
 	}
 	protected function configure(){
+		$aliases = [];
+		foreach(static::TASKS as $task){
+			$aliases[] = 'build:' . $task;
+		}
 		$this
 			->setName('build')
-			->setDescription("Build site.")
+			->setAliases($aliases)
+			->setDescription("Run site build task(s).  Run all with `build` command, one with `build:*` command.")
 			->addOption('dist', 'd', InputOption::VALUE_REQUIRED, 'Which dist folder to build to.  May also change some characteristics of how build is done', 'public')
 			//-! arg: dest (folder to build into)
 			//-! opt: host type (eg apache, github pages, cloudflare pages)
@@ -24,12 +37,35 @@ class BuildCommand extends Command{
 		;
 	}
 	protected function execute(InputInterface $input, OutputInterface $output): int{
-		$this->buildService->buildStaticPages($input->getOption('dist'));
-		$this->buildService->installWebRootFiles($input->getOption('dist'));
-		//-! should eventually support doing all build steps
+		$command = explode(':', $input->getArgument('command'));
+		if(count($command) === 1){
+			$tasks = ['assets', 'css', 'js', 'static', 'svg'];
+		}else{
+			$tasks = [$command[1]];
+		}
+		foreach($tasks as $task){
+			switch($task){
+				case 'assets':
+					$this->buildService->linkAssets($input->getOption('dist'));
+				break;
+				case 'css':
+					$this->buildService->buildCSS($input->getOption('dist'), $output);
+				break;
+				case 'js':
+					$this->buildService->buildJS(null, $input->getOption('dist'), $output);
+				break;
+				case 'static':
+					$this->buildService->buildStaticPages($input->getOption('dist'));
+				break;
+				case 'svg':
+					$this->buildService->buildSvgs($input->getOption('dist'));
+				break;
+				case 'webroot':
+					$this->buildService->installWebRootFiles($input->getOption('dist'));
+				break;
+			}
+		}
 		//-! should eventually support different host targets
-		// $this->buildService->linkAssets();
-		// $this->buildService->buildSvgs();
 		return 0;
 	}
 }
