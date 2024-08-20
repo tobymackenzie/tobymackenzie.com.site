9c11171d4594b72910140b682f38b3a2686ab8a6
Toby Mackenzie <public@tobymackenzie.com>
2024-08-20 16:53:57 -0400

    fix: github friendly URLs weren't working

--- src/PublicApp/Listener/ViewContentListener.php
+++ src/PublicApp/Listener/ViewContentListener.php
@@ -16,8 +16,26 @@ class ViewContentListener{
 		$content = $event->getContent();
 		$path = $event->getPath();
 		//--support github friendly URLs in content repo
-		$content = preg_replace(':(href=["\'])/content/:', '$1' . $this->router->generate('public_home'), $content);
+		//-! should we just do this as part of wiki-site?
+		$router = $this->router;
 		$format = pathinfo($path, PATHINFO_EXTENSION) ?? 'html';
+		$content = preg_replace_callback(':(href=["\'])/content/([^\'"]*):', function($matches) use($format, $router){
+			$result = $matches[1] . $router->generate('public_home');
+			//--must strip off / replace trailing '.md', needed for github URLs
+			if(pathinfo($matches[2], PATHINFO_EXTENSION) === 'md'){
+				$result .= substr($matches[2], 0, -3);
+				if($format !== 'html'){
+					if($matches[2] === '/'){
+						$result .= 'index.' . $format;
+					}else{
+						$result .= '.' . $format;
+					}
+				}
+			}else{
+				$result .= $matches[2];
+			}
+			return $result;
+		}, $content);
 		if($format === 'md' || $format === 'txt'){
 			$content = preg_replace(':(\]\()/content/:', '$1' . $this->router->generate('public_home', [], UrlGeneratorInterface::ABSOLUTE_URL), $content);
 		}
