dbe955df8ec7122ffb5f6c5d970e3143d7f77c65
Toby Mackenzie <public@tobymackenzie.com>
2025-03-14 14:05:48 -0400

    whimsy(aprilfools): change joke.
    modify words in page to be more funny / weird

--- src/PublicApp/scripts/aprilFools.js
+++ src/PublicApp/scripts/aprilFools.js
@@ -1,25 +1,222 @@
-if(!(window.matchMedia && matchMedia('(prefers-reduced-motion: reduce)').matches)){
-	var doc = document;
-	var $el = doc.querySelector('html');
-	var style = $el.style;
-	var orgPosition = style.position;
-	var orgTransform = style.transform;
-	var orgTransformOrigin = style.transformOrigin;
-	style.transition += ' transform 0.5s ease';
-	style.position = 'relative';
-	style.transform = 'scaleX(-1)';
-	style.transformOrigin = 'center';
-	var done = function(){
-		style.transform = 'scaleX(1)';
-		doc.removeEventListener('click', done);
-		doc.removeEventListener('mousemove', done);
-		doc.removeEventListener('scroll', done);
-		console.log('april fools');
-		style.position = orgPosition;
-		style.transform = orgTransform;
-		style.transformOrigin = orgTransformOrigin;
+function getRandItem(arr){
+	return arr[Math.floor(Math.random() * arr.length)]
+};
+//-@ https://stackoverflow.com/a/17265031
+function replaceStringInNode(node, find, replacement){
+	if(node.nodeType === 3 && node.textContent){
+		if(typeof find === 'string'){
+			find = new RegExp(find, 'gi');
+		}
+		node.textContent = node.textContent.replace(find, function(match){
+			var str = '';
+			for(var i = 0; i < match.length; ++i){
+				var char = match.charAt(i);
+				var replaceChar = replacement.charAt(i);
+				var mutate = 'no';
+				if(char.match(/[A-Z]/)){
+					var mutate = 'up';
+					str += replaceChar.toUpperCase();
+				}else if(char.match(/[a-z]/)){
+					var mutate = 'down';
+					str += replaceChar.toLowerCase();
+				}else{
+					str += replaceChar;
+				}
+			}
+			if(match.length < replacement.length){
+				str += replacement.substr(match.length);
+			}
+			return str;
+		});
+	}else{
+		for(var sub in node.childNodes){
+			replaceStringInNode(node.childNodes[sub], find, replacement);
+		}
+	}
+}
+function replaceStringsInNode(node, arr){
+	for(var i = 0; i < arr.length; ++i){
+		replaceStringInNode(node, arr[i][0], arr[i][1]);
+	}
+}
+
+//==main
+var doc = document;
+if(doc.querySelector){
+	var main = function($el){
+		if(!$el){
+			$el = doc.body;
+		}
+
+		//--funny text replacement
+		replaceStringsInNode($el, [
+			['tobymackenzie.com', 'macn.me'],
+			['blog', 'web-pipe'],
+			['toby', 'boby'],
+			['mackenzie', 'mackerby'],
+			['internet', 'information superhighway'],
+			[/\bweb developer\b/gi, 'webmaster of the universe'],
+			['god', 'dog'],
+			[/\boh boy\b/, 'oh buoy'],
+			//--archaic
+			[/\benjoy\b/gi, 'enjoyst'],
+			[/\bhello\b/gi, 'ahoy'],
+			[/\bmate\b/gi, 'matey'],
+			[/\byou\b/gi, 'thou'],
+			[/\byour\b/gi, 'thy'],
+			//--brit
+			[/\bmy\b/gi, 'me'],
+			[/\bsomething\b/gi, 'summat'],
+			//--canuck
+			['about', 'aboot'],
+			[/\bout\b/, 'oot'],
+			//--fic
+			[/\bcool\b/gi, 'shiny'],
+			['friend', 'droog'],
+			//--places
+			['Akron', 'Rubber City'],
+			['Cleveland', 'Bomb City'],
+			['Cuyahoga Falls', 'Caucasian Falls'],
+			['Ohio', 'Great River'],
+			//--scots
+			[/\bcannot\b/gi, 'cannae'],
+			[/\bcan't\b/gi, 'cannae'],
+			[/\bhaven't\b/gi, 'huvnae'],
+			[/\blittle\b/gi, 'wee'],
+			[/\boy\b/gi, 'lad'],
+			[/\bgirl\b/gi, 'lass'],
+			['know', 'ken'],
+			[/\bsmall\b/gi, 'wee'],
+			[/\bno\b/gi, 'nae'],
+			[/\bwon't\b/gi, 'winnae'],
+			[/\byes\b/gi, 'aye'],
+		]);
+
+		//--funny intro / outro
+		var $firstP = doc.querySelector('p');
+		if($firstP){
+			var phrases = [
+				'Here ye, here ye!  ',
+				'Abraham Lincoln once said:  ',
+				'Ahem.  Is this thing on?  Okay.  ',
+				'Ahoy matey!  ',
+				'All your base are belong to us!  ',
+				// 'As you wish.  ',
+				'Attention, web surfer!  ',
+				'Awooga!  ',
+				'Beltalowda!  ',
+				'Bork bork bork!  ',
+				'Breaking news.  ',
+				'Comrade!  ',
+				'Don\'t be ignorant.  ',
+				'Dear web surfer, ',
+				'Dude!  ',
+				'Foolish person.  ',
+				'Friends!  Romans!  Lend me your eyes!  ',
+				'Get your facts straight.  ',
+				'Heed!  ',
+				'Hello, [insert name here].  ',
+				'Hi!  I\'m Boby Mackerby.  You may remember me from such websites as Cogneato.com and How to Mothball Your Battleship.  ',
+				'Hit it!  ',
+				'Humanoids, hark!  ',
+				'I do declare ',
+				'I reckon ',
+				'It was the best of times.  It was the blurst of times.  ',
+				'Ladies and gentlemen!  The one, the only, Boby Mackerby! … Thank you!  Thank you!  ',
+				'Listen up.  ',
+				'Make haste!  ',
+				'Mark me!  ',
+				'My fellow humanoids!  ',
+				'My, how the turntables.  ',
+				'No way!  ',
+				'Now who could that be?  ',
+				'Right!  ',
+				'Righteous!  ',
+				'On a chilly night in April, ',
+				'Once upon a time ',
+				'Oy!  ',
+				'Smeg!  ',
+				'This is god.  ',
+				'Three two one.  One two three.  What the heck is bothering me?  ',
+				'Warning!  Warning!  ',
+				'What up, ma nerds?  ',
+				// 'Who are you?  Oh.  I see.  ',
+				'You fool!  ',
+				'You\'re not going to believe this!  ',
+				'Welcome, gentle reader.  ',
+			];
+			$firstP.childNodes[0].textContent = getRandItem(phrases) + $firstP.childNodes[0].textContent;
+		}
+		var $main = doc.querySelector('main .mainItem:last-of-type');
+		if(!$main){
+			$main = doc.querySelector('main');
+		}
+		if($main){
+			var phrases = [
+				'All hail King Bluetooth!',
+				'And he trailed off into the sunset.',
+				'And just like that, as quickly as it had started, it was finished.',
+				'And no one was the wiser.',
+				'And now we\'re in a dilly of a pickle.',
+				'And so it goes.',
+				'And that is the word of god.',
+				'And that will be our downfall.',
+				'April Fools!',
+				'Be excellent to each other, and party on dudes!',
+				'Buck up, little camper:  We\'ll beat that slope… together.',
+				'But hey, it could be verse.',
+				'[Exit stage left]',
+				'Fin',
+				'For the greater good!',
+				'From this, the obvious conclusion is that birds aren\'t real.',
+				'Good day!  I said good day!',
+				'Good luck!',
+				'Got ya!',
+				'Heed this message, and you will be saved.',
+				'I plum told ya',
+				'Like, totally!',
+				'Me want cookies.',
+				'My balls are now in your court.',
+				'Now go do that voodoo that you do.',
+				'Now go save the world!',
+				'Now go, you fool!',
+				'Now you try.',
+				'Prove me wrong.',
+				'Reporing live from macn.me, this is Boby Mackerby, signing off.',
+				'Seize the day!',
+				'So suck it.',
+				'That\'s all folks!',
+				'The end!',
+				'Think about it.',
+				'This information is classified.',
+				'This message will self destruct.',
+				'To arms!',
+				'Use this information wisely.',
+				'Vague but exciting.',
+				'We can obviously conclude that birds are CIA spy drones.',
+				'Who knew?',
+				'Ya ken?',
+				'You\'ll see.',
+				'Your hero in arms, Boby Mackerby',
+				'Your turn.',
+			];
+			var $p = document.createElement('p');
+			$p.innerText = getRandItem(phrases);
+			var $footer = $main.querySelector('footer');
+			var done = false;
+			var i = 0;
+			while(!done && ++i < 10){
+				try{
+					$main.insertBefore($p, $footer);
+				}catch(e){
+					$footer = $footer.parentNode;
+				}
+			}
+		}
 	};
-	doc.addEventListener('click', done);
-	doc.addEventListener('mousemove', done);
-	doc.addEventListener('scroll', done);
+	if(doc.readyState !== 'loading'){
+		main();
+	}else{
+		doc.addEventListener('DOMContentLoaded', main);
+	}
 }
