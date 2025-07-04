88fac01fe48400bf4bdf623db353d50bdeceef4d
Toby Mackenzie <public@tobymackenzie.com>
2025-07-03 23:07:08 -0400

    fix(js): better old browser compat for extra.js

--- src/PublicApp/scripts/ua/onday.js
+++ src/PublicApp/scripts/ua/onday.js
@@ -2,7 +2,7 @@ export default function onday(cb){
 	cb();
 	var then = new Date();
 	then.setHours(24, 0, 0, 0);
-	var diff = then - Date.now();
+	var diff = then - (new Date());
 	if(diff <= 100){ diff = 360000 };
 	setTimeout(function(){ onday(cb) }, diff);
 };
