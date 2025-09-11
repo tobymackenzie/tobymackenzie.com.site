744a7f3bd452d25e75b37b1d7a31370278a7aeba
Toby Mackenzie <public@tobymackenzie.com>
2025-09-11 00:36:31 -0400

    feat(blog): better match 404 content to 404 of non-blog site

--- src/Blog/theme/content-none.php
+++ src/Blog/theme/content-none.php
@@ -16,14 +16,19 @@ if(WP_DEBUG){
 ?>
 <article class="post post-none post-0 mainItem" id="post-0">
 	<header class="postHeader">
+<?php if(is_search()){ ?>
 		<h1 class="postHeading"><?php _e('Nothing found', 'tmweb'); ?></h1>
+<?php }else{ ?>
+		<h1 class="postHeading"><?php _e('Error 404: Not Found', 'tmweb'); ?></h1>
+<?php } ?>
 	</header>
 	<div class="postContent">
 <?php if(is_search()){ ?>
 		<p class="notice"><?php _e('Sorry, but your search did not match anything on this site. Please try a different search.', 'tmweb'); ?></p>
 <?php }else{ ?>
-		<p><?php _e('Sorry, but what you are looking for could not be found. Perhaps you can find what you are looking for with a search.', 'tmweb'); ?></p>
+		<p><?php _e('The page you requested was not found.  Try searching for it with the form below, or peruse the <a href="/">home page</a>.', 'tmweb'); ?></p>
 <?php } ?>
+		<br />
 		<?php get_search_form(); ?>
 	</div>
 </article>
