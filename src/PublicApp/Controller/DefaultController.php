4717b4d165c42c56d8e41e1a4a77076346a77560
Toby Mackenzie <public@tobymackenzie.com>
2025-10-03 15:24:32 -0400

    code: move alias logic into controller action to simplify

--- src/PublicApp/Controller/DefaultController.php
+++ src/PublicApp/Controller/DefaultController.php
@@ -5,14 +5,12 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use TJM\Pages\Pages;
 use TJM\WikiSite\WikiSite;
 
 class DefaultController extends Controller{
 	const SUPPORTED_FORMATS = [null, 'html', 'md', 'txt', 'xhtml'];
 	public function pageAction(
 		$id
-		,Pages $pagesService
 		,Request $request
 		,WikiSite $wikiSite
 		,$_format = null
@@ -20,12 +18,19 @@ class DefaultController extends Controller{
 		//-!! temporarily pass through to new action with fall back to old logic until we have redirects implemented properly
 		try{
 			return $wikiSite->viewAction($_format && $_format !== 'html' ? $id . '.' . $_format : $id);
-		}catch(\Exception $e){ }
-		//==old logic for redirects
-		$page = $pagesService->getResponse(strtolower($id), $_format);
-		if($page && $page->getType() === 'redirect'){
-			return $this->redirect($page->getContent());
+		}catch(\Exception $e){
+			$aliases = json_decode(file_get_contents(__DIR__ . '/../../../data/aliases.json'), true);
+			$alias = $aliases[$id] ?? null;
+			if($alias === null){
+				throw $this->createNotFoundException();
+			}
+			if(strpos($alias, '/') === false){
+				$alias = '/' . $alias;
+			}
+			if($_format && $_format !== 'html'){
+				$alias .= ".{$_format}";
+			}
+			return $this->redirect($alias);
 		}
-		throw $this->createNotFoundException();
 	}
 }
