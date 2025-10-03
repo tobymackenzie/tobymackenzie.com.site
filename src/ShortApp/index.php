082c1824740e39d80a499b76680c7059eb33be30
Toby Mackenzie <public@tobymackenzie.com>
2025-10-03 16:25:11 -0400

    code(short): move verbose PHP code from template to make template easier to read

--- src/ShortApp/index.php
+++ src/ShortApp/index.php
@@ -1,23 +1,6 @@
 <?php
-use chillerlan\QRCode\QRCode;
-use chillerlan\QRCode\Data\QRMatrix;
-use chillerlan\QRCode\QROptions;
 require_once(__DIR__ . '/inc.php');
-require_once(AUTOLOAD_PATH);
-$qr = (new QRCode(new QROptions([
-	'addQuietzone'=> false,
-	//'outputType'=> QRCode::OUTPUT_MARKUP_SVG,
-	//'imageBase64'=> false,
-	'scale'=> 2,
-	'xmoduleValues'=> [
-		1024=> [144,223,144],
-		QRMatrix::M_ALIGNMENT=> [209, 255, 209],
-		QRMatrix::M_FINDER=> [209, 255, 209],
-		QRMatrix::M_FORMAT=> [144,223,144],
-		QRMatrix::M_TIMING=> [144,223,144],
-
-	],
-])));
+require_once(__DIR__ . '/qr.php');
 ?>
 <!doctype html>
 <html lang="en">
@@ -77,20 +60,6 @@ __     __    _      ______ ._.    __._______.._    ._. _______.._______.._______
 				</div>
 				<div class="cardItem"><a href="//www.tobymackenzie.com/blog/">tobymackenzie.com/blog</a></div>
 				<div class="cardItem"><a href="https://github.com/tobymackenzie">github.com/tobymackenzie</a></div>
-<?php
-$vCard = 'BEGIN:VCARD
-VERSION:4.0
-ADR:;;;Akron;Ohio;;USA
-CATEGORIES:CSS,HTML,JavaScript,PHP
-FN:Toby Mackenzie
-EMAIL:public@tobymackenzie.com
-N:Mackenzie;Toby
-PHOTO:https://macn.me/_toby.jpg
-TITLE:Webmaster
-URL:https://www.tobmackenzie.com
-URL:https://macn.me
-END:VCARD';
-?>
 				<img class="qr" src="<?=$qr->render($vCard)?>" alt="QR code: vCard" />
 				<div class="appHeaderMessage"><a href="//bxln2ho3.com">Be excellent to each other</a></div>
 			</div>
---
+++ src/ShortApp/qr.php
@@ -0,0 +1,33 @@
+<?php
+use chillerlan\QRCode\QRCode;
+use chillerlan\QRCode\Data\QRMatrix;
+use chillerlan\QRCode\QROptions;
+require_once(AUTOLOAD_PATH);
+$qr = (new QRCode(new QROptions([
+	'addQuietzone'=> false,
+	//'outputType'=> QRCode::OUTPUT_MARKUP_SVG,
+	//'imageBase64'=> false,
+	'scale'=> 2,
+	'xmoduleValues'=> [
+		1024=> [144,223,144],
+		QRMatrix::M_ALIGNMENT=> [209, 255, 209],
+		QRMatrix::M_FINDER=> [209, 255, 209],
+		QRMatrix::M_FORMAT=> [144,223,144],
+		QRMatrix::M_TIMING=> [144,223,144],
+
+	],
+])));
+$vCard = 'BEGIN:VCARD
+	VERSION:4.0
+	ADR:;;;Akron;Ohio;;USA
+	CATEGORIES:CSS,HTML,JavaScript,PHP
+	FN:Toby Mackenzie
+	EMAIL:public@tobymackenzie.com
+	N:Mackenzie;Toby
+	PHOTO:https://macn.me/_toby.jpg
+	TITLE:Webmaster
+	URL:https://www.tobmackenzie.com
+	URL:https://macn.me
+	END:VCARD'
+;
+
