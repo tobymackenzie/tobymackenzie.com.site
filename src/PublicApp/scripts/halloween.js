58785b64ebad5a3e20c23f2b361a1cc55828b61a
Toby Mackenzie <public@tobymackenzie.com>
2025-10-22 19:25:51 -0400

    code: use repo version of ghosts.js

--- src/PublicApp/scripts/halloween.js
+++ src/PublicApp/scripts/halloween.js
@@ -1,14 +1,1 @@
-import {ready as _ready} from './modules/snow/ready.js';
-// import {CanvasGhostView as GhostView} from './modules/ghost/canvasGhostView.js';
-import {SVGGhostView as GhostView} from './modules/ghost/svgGhostView.js';
-
-if(
-	document.querySelector
-	&& window.requestAnimationFrame
-	&& !(window.matchMedia && window.matchMedia('(prefers-reduced-motion: reduce)').matches)
-){
-	_ready(function(){
-		var _view = new GhostView();
-		_view.activate();
-	});
-}
+import './lib/@tobymackenzie/ghosts/src/ghosts.js';
