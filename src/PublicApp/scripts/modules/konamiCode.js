7c748de6f862ca40b7d3ffa058489ef4ae26028e
Toby Mackenzie <public@tobymackenzie.com>
2025-10-22 21:16:02 -0400

    whimsy: timeout for konami code, must enter reasonably fast

--- src/PublicApp/scripts/modules/konamiCode.js
+++ src/PublicApp/scripts/modules/konamiCode.js
@@ -6,20 +6,24 @@ import loadJS from '../ua/load-js.js';
 if(_cutsMustard){
 	var _current = 0;
 	var _keys = [38,38,40,40,37,39,37,39,66,65];
+	var _timeout;
+	var _reset = function(){
+		_current = 0;
+	};
 	_addListener(document, 'keyup', function(_event){
 		var _key =  _event.which || _event.keyCode;
+		clearTimeout(_timeout);
 		if(_key === _keys[_current]){
 			if(_current === 9){
-				_current = 0;
+				_reset();
 				loadJS('/_assets/scripts/aprilFools.js');
 				alert('Be excellent to each other… and party on, dudes');
 			}else{
+				_timeout = setTimeout(_reset, 1200);
 				++_current;
 			}
-		}else{
-			if(!(_current <= 2 && _key === 38)){
-				_current = 0;
-			}
+		}else if(!(_current <= 2 && _key === 38)){
+			_reset();
 		}
 	});
 }
