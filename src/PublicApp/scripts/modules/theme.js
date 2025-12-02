58a968ff62b88fe015a814725192be9b1ee094fb
Toby Mackenzie <public@tobymackenzie.com>
2025-12-01 23:53:15 -0500

    code: disable ancient stylesheet if theming to simplify new themes
    eg don't require undoing ancient theme

--- src/PublicApp/scripts/modules/theme.js
+++ src/PublicApp/scripts/modules/theme.js
@@ -3,9 +3,16 @@ import switchTheme from '../lib/@tobymackenzie/theme-switch/src/switcher.js';
 switchTheme(
 	{
 		'main': 'Default',
-		'': 'Fallback'
+		'ancient': 'Fallback'
 	},
 	'<a--navi><button type="button" class="a--navsta a--nava" title="Switch theme"><a--navt>Theme</a--navt></button></a--navi>',
 	'a--navi:last-child',
 	'insertBefore'
 );
+
+//--disable ancient if we're themeing
+//-# not for default, since we want to ensure no-js browsers work fine
+if(window.localStorage && localStorage.getItem('tjm-theme')){
+	var ancientStyles = document.querySelector('link[rel="stylesheet"]');
+	ancientStyles.rel = 'x';
+}
