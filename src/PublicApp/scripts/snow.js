4fc9e2b2c4cf13b401152a111e1c709aa12fb67c
Toby Mackenzie <public@tobymackenzie.com>
2025-12-04 21:51:29 -0500

    perf(snow): use HTML transform snow view, seems to perform better now

--- src/PublicApp/scripts/snow.js
+++ src/PublicApp/scripts/snow.js
@@ -1,5 +1,9 @@
-import {ready as _ready} from './modules/snow/ready.js';
-import {CanvasSnowView} from './modules/snow/canvasSnowView.js';
+// import {ready as _ready} from './modules/snow/ready.js';
+import {ready as _ready} from './@tobymackenzie/ready.js';
+// import {CanvasSnowView as SnowView} from './modules/snow/canvasSnowView.js';
+// import {HTMLPosSnowView as SnowView} from './modules/snow/htmlPosSnowView.js';
+import {HTMLTransformSnowView as SnowView} from './modules/snow/htmlTransformSnowView.js';
+// import {SVGSnowView as SnowView} from './modules/snow/svgSnowView.js';
 
 if(
 	document.querySelector
@@ -7,7 +11,7 @@ if(
 	&& !(window.matchMedia && window.matchMedia('(prefers-reduced-motion: reduce)').matches)
 ){
 	_ready(function(){
-		var _view = new CanvasSnowView();
+		var _view = new SnowView();
 		_view.activate();
 	});
 }
