586000ab6b631aa32fed13123010d94c2f5843e3
Toby Mackenzie <public@tobymackenzie.com>
2025-12-05 23:18:45 -0500

    feat: add some classless third party themes to theme switcher

--- src/PublicApp/scripts/modules/theme.js
+++ src/PublicApp/scripts/modules/theme.js
@@ -4,7 +4,14 @@ switchTheme(
 	{
 		'15': 'Default',
 		'stark': 'Stark',
-		'ancient': 'Fallback'
+		'ancient': 'Fallback',
+		'Third Party': {
+			'https://unpkg.com/magick.css': 'Magick CSS',
+			'https://unpkg.com/papercss@1.9.2/dist/paper.min.css': 'Paper CSS',
+			'https://cdn.simplecss.org/simple.min.css': 'Simple CSS',
+			'https://www.w3.org/StyleSheets/Core/Chocolate': 'W3C Chocolate',
+			'https://www.w3.org/StyleSheets/Core/Modernist': 'W3C Modernist'
+		}
 	},
 	'<a--navi><button type="button" class="a--navsta a--nava" title="Switch theme"><a--navt>Theme</a--navt></button></a--navi>',
 	'a--navi:last-child',
