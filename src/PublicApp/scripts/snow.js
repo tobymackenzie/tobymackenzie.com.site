4c82b69143255d8c157f3186f5d29fda3b3a1a44
Toby Mackenzie <public@tobymackenzie.com>
2026-02-12 16:04:49 -0500

    code: move snow.js to external repo

--- src/PublicApp/scripts/snow.js
+++ src/PublicApp/scripts/snow.js
@@ -1,17 +1,1 @@
-// import {ready as _ready} from './modules/snow/ready.js';
-import {ready as _ready} from './@tobymackenzie/ready.js';
-// import {CanvasSnowView as SnowView} from './modules/snow/canvasSnowView.js';
-// import {HTMLPosSnowView as SnowView} from './modules/snow/htmlPosSnowView.js';
-import {HTMLTransformSnowView as SnowView} from './modules/snow/htmlTransformSnowView.js';
-// import {SVGSnowView as SnowView} from './modules/snow/svgSnowView.js';
-
-if(
-	document.querySelector
-	&& window.requestAnimationFrame
-	&& !(window.matchMedia && window.matchMedia('(prefers-reduced-motion: reduce)').matches)
-){
-	_ready(function(){
-		var _view = new SnowView();
-		_view.activate();
-	});
-}
+import snow from './lib/@tobymackenzie/snow/src/snow.js';
