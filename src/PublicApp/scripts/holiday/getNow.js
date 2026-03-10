ad30ff6ab7e7d2926cbd5602a97fffe19cb32254
Toby Mackenzie <public@tobymackenzie.com>
2026-03-10 15:28:55 -0400

    code: missing semicolons

--- src/PublicApp/scripts/holiday/getNow.js
+++ src/PublicApp/scripts/holiday/getNow.js
@@ -1,7 +1,7 @@
 export default function getNow(){
 	//-# uncomment, specify date in URL to test dates
 	dev: {
-		var parm
+		var parm;
 		if(window.URLSearchParams && (parm = new URLSearchParams(location.search)) && parm.has('now')){
 			var now = parm.get('now');
 			if(now.length === 8){
@@ -13,7 +13,7 @@ export default function getNow(){
 				var m = now.substr(0, 2) - 1;
 				var d = now.substr(2, 2);
 			}
-			return new Date(y, m, d, 0, 0, 0)
+			return new Date(y, m, d, 0, 0, 0);
 		}
 	}
 	return new Date();
