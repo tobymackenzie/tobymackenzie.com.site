214884a7235e9995330128d33faa3c769ff02770
Toby Mackenzie <public@tobymackenzie.com>
2026-07-15 11:29:52 -0400

    conf: remove gone dev ips setting

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -7,7 +7,6 @@ const PROJECT_DIR = APP_DIR;
 require(PROJECT_DIR . '/vendor/autoload.php');
 //--build app
 $app = new App([
-	'allowedDevIPs'=> ['127.0.0.1', '::1', '192.168.56.1'],
 	'bundles'=> [
 		'Symfony\Bundle\FrameworkBundle\FrameworkBundle',
 		'Symfony\Bundle\TwigBundle\TwigBundle',
